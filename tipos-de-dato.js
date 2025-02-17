c33d2c638c23c6afee391f2521041d239260ee3a
darvfus <romerondan123@gmail.com>
2025-02-17 12:30:24 -0600

    first commit

---
+++ tipos-de-dato.js
@@ -0,0 +1,16 @@
+console.log(typeof 42);            
+console.log(typeof 'Veinticinco'); 
+console.log(typeof -666);          
+console.log(typeof true);          
+console.log(typeof 0);             
+console.log(typeof '');            
+console.log(typeof null);          
+console.log(typeof undefined);     
+console.log(typeof FALSE);         
+
+// Ejemplos adicionales
+console.log(typeof 3.14);         
+console.log(typeof []);           
+console.log(typeof {});           
+console.log(typeof function(){}); 
+console.log(typeof NaN);          
